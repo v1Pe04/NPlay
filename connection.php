4e66ef8d6e9754ad4a56849abb54c6608faf1ff9
Viljar Pettersen <87280386+v1Pe04@users.noreply.github.com>
2022-10-28 10:51:06 +0200

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,19 +1,37 @@
-<?php
-// Laget av Viljar
-
-// tast din host , user, passord og databasen 
-// Vi oppretter variabel under for connection
-
-$host="localhost";
-$user="id17230195_nuutxd";
-$pass="Mog/OgDeg123";
-$db="id17230195_pirat";
-
-
-$connect=mysqli_connect($host, $user, $pass, $db) or die("Koblingen feil");
-
-
-
-
-
+<?php
+
+// Laget av Viljar
+
+
+
+// tast din host , user, passord og databasen 
+
+// Vi oppretter variabel under for connection
+
+
+
+$host="localhost";
+
+$user="";
+
+$pass="";
+
+$db="";
+
+
+
+
+
+$connect=mysqli_connect($host, $user, $pass, $db) or die("Koblingen feil");
+
+
+
+
+
+
+
+
+
+
+
 ?>
